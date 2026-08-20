6d1bb650635b5326b9c0d5212d9178d19a47798b
adityatathe <adityatathe9891@gmail.com>
2026-08-20 13:53:26 +0530

    this is the code to move zero's to end of the array

---
+++ Move0ToLast.java
@@ -0,0 +1,46 @@
+package com.arr;
+
+import java.util.Scanner;
+
+public class Move0ToLast {
+
+	public static int[] movzerotoLast(int[] arr) {
+		// TODO Auto-generated method stub
+
+		int index = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != 0) {
+				arr[index] = arr[i];
+				index++;
+			}
+			
+		}
+		while(index<arr.length) {
+			arr[index]=0;
+			index++;
+		}
+		return arr;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int size;
+		System.out.println("Enter size of array");s
+		size = sc.nextInt();
+		int[] arr1 = new int[size];
+
+		System.out.println("Enter the values one by one");
+		for (int i = 0; i < size; i++) {
+			arr1[i] = sc.nextInt();
+		}
+
+		int[] temp = movzerotoLast(arr1);
+		for (int i : temp) {
+
+			System.out.print(i + " \t ");
+		}
+	}
+
+}
